62a4a354dc42efd4018dd64d68536df7fd28a115
bogutskii <sl9site@gmail.com>
2023-07-17 12:48:06 -0700

    change design show popup when copy in clipboard fix errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const SpeechRecognition =
     window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -12,6 +13,8 @@ function App() {
   const [finalTranscript, setFinalTranscript] = useState("");
   const [interimTranscript, setInterimTranscript] = useState("");
   const [isListening, setIsListening] = useState(false);
+  const [showToast, setShowToast] = useState(false);
+  const [microphoneError, setMicrophoneError] = useState(false);
 
   useEffect(() => {
     recognition.onresult = function (event) {
@@ -32,51 +35,86 @@ function App() {
         recognition.start();
       }
     };
+
+    recognition.onnomatch = function () {
+      setMicrophoneError(true);
+    };
+
+    recognition.onerror = function (event) {
+      setMicrophoneError(true);
+    };
   }, [isListening]);
 
-  const startListening = () => {
-    if (!isListening) {
+  const toggleListening = () => {
+    if (isListening) {
+      setIsListening(false);
+      recognition.stop();
+    } else {
       setIsListening(true);
       recognition.start();
     }
   };
 
-  const stopListening = () => {
-    if (isListening) {
-      setIsListening(false);
-      recognition.stop();
-    }
-  };
-
   const resetTranscript = () => {
-    stopListening();
+    setIsListening(false);
     setFinalTranscript("");
     setInterimTranscript("");
+    setMicrophoneError(false);
+    recognition.stop();
   };
 
   const copyToClipboard = () => {
     const textToCopy = finalTranscript.trim();
-
     const textarea = document.createElement("textarea");
     textarea.value = textToCopy;
     document.body.appendChild(textarea);
     textarea.select();
     document.execCommand("copy");
     document.body.removeChild(textarea);
+
+    setShowToast(true);
+    setTimeout(() => {
+      setShowToast(false);
+    }, 500);
   };
 
   return (
-      <div>
-        <button onClick={startListening}>Start</button>
-        <button onClick={stopListening}>Stop</button>
-        <button onClick={resetTranscript}>Reset</button>
-        <button onClick={copyToClipboard}>Copy Text</button>
-        <div>
-          <p>{finalTranscript}</p>
-          <p>{interimTranscript}</p>
+      <div className="app-container">
+        <div className="title">Speech Recognition Extension</div>
+        <div className="button-container">
+          <button
+              className={`glow-button ${isListening ? "stop" : "start"}`}
+              onClick={toggleListening}
+          >
+            {isListening ? "Stop" : "Start"}
+          </button>
+          <button className="glow-button reset" onClick={resetTranscript}>
+            Reset
+          </button>
+          <button className="glow-button copy" onClick={copyToClipboard}>
+            Copy Text
+          </button>
         </div>
+        {microphoneError && (
+            <div className="microphone-error">
+              Microphone is unavailable. Please check your microphone settings.
+            </div>
+        )}
+        <div className="transcript-container">
+        <textarea
+            className="transcript-text"
+            value={finalTranscript}
+            onChange={(e) => setFinalTranscript(e.target.value)}
+        ></textarea>
+        </div>
+        <div className="interim-transcript">{interimTranscript}</div>
+        {showToast && <Toast message="Text copied to clipboard" />}
       </div>
   );
+}
+
+function Toast({ message }) {
+  return <div className="toast">{message}</div>;
 }
 
 export default App;
