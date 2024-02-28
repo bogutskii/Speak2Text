2158673da11084688e900cd41d4b5e5fcc67d03c
bogutskii <sl9site@gmail.com>
2024-02-28 10:36:31 -0800

    add About page

--- src/components/Header/LanguageInterfaceToggle.js
+++ src/components/Header/LanguageInterfaceToggle.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { connect } from 'react-redux';
 import {
   setInterfaceLanguage,
@@ -13,7 +13,9 @@ const LanguageInterfaceToggle = ({
   setCurrentInterface,
 }) => {
   const [selectedLanguage, setSelectedLanguage] = useState(currentInterfaceLanguage);
-  const languages = { en: 'Eng', ukr: 'Ukr', ru: 'Rus' };
+  const [isDropdownExpanded, setIsDropdownExpanded] = useState(false);
+  const languages = { en: ['EN', 'English'], ukr: ['UA', 'Ukrainian'], ru: ['RU', 'Russian'] };
+  const selectRef = useRef(null);
 
   const handleInterfaceChange = (event) => {
     const selectedValue = event.target.value;
@@ -21,14 +23,36 @@ const LanguageInterfaceToggle = ({
     setInterfaceLanguage(selectedValue);
     loadLanguageFile(selectedValue);
     setCurrentInterface(selectedValue);
+    setIsDropdownExpanded(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (selectRef.current && !selectRef.current.contains(event.target)) {
+        setIsDropdownExpanded(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [selectRef]);
+  const handleFocus = () => {
+    setIsDropdownExpanded(true);
   };
 
   return (
     <div className="interface-language-toggle">
-      <select className='interface-language-select' value={selectedLanguage} onChange={handleInterfaceChange}>
+      <select
+        className='interface-language-select'
+        value={selectedLanguage}
+        onChange={handleInterfaceChange}
+        onFocus={handleFocus}
+      >
         {Object.entries(languages).map(([key, value]) => (
           <option key={key} value={key}>
-            {value}
+            {isDropdownExpanded ? value[1] : value[0]}
           </option>
         ))}
       </select>
